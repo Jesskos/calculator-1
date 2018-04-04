75649ff42b72da93a8e9d7f21954e62e2a532f1f
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-04 12:20:06 -0700

    Final caculator after code review

--- arithmetic.py
+++ arithmetic.py
@@ -22,12 +22,14 @@ def multiply(num1, num2):
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
 
-    return num1/num2
+    return num1 / num2
+
 
 def square(num1):
     """Return the square of the input."""
-    
+
     return num1**2
+   
 
 def cube(num1):
     """Return the cube of the input."""
@@ -45,3 +47,4 @@ def mod(num1, num2):
     """Return the remainder of num1 / num2."""
 
     return num1 % num2
+
